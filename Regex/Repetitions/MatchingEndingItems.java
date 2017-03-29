762256c840d76322ff1fb5fbb7bec89391b4a2f8
Omri Gotlieb <omrigotlieb@gmail.com>
2017-03-29 16:26:14 -0700

    Deleted comments

--- Regex/Repetitions/MatchingEndingItems.java
+++ Regex/Repetitions/MatchingEndingItems.java
@@ -1,9 +1,7 @@
 public class Solution {    
 
     public static void main(String[] args) {
-        
         Regex_Test tester = new Regex_Test();
         tester.checker("^[a-zA-Z]*[s]$"); 
-    
     }
 }
