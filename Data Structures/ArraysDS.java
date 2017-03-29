1f88c3b0d0ccafe35ef6381c1f21ac5f86b8fd0a
Omri Gotlieb <omrigotlieb@Omris-MacBook-Pro.local>
2017-03-28 20:20:59 -0700

    Arrays - DS Solution added

---
+++ Data Structures/ArraysDS.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int size = in.nextInt();
+        int[] arr = new int[size];
+        for(int i = 0; i<size; i++){
+            arr[i] = in.nextInt();
+        }
+        for(int j = size - 1; j >= 0; j--){
+            System.out.print(arr[j] + " ");
+        }
+        in.close();
+    }
+}
