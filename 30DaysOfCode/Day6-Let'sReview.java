28bb5d589dc795e3a91d0f9c8d7b9d2297dda859
Omri Gotlieb <omrigotlieb@gmail.com>
2017-04-05 20:03:50 -0700

    Added 30 days of code
    Added day 3 and 6

---
+++ 30DaysOfCode/Day6-Let'sReview.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+            in.nextLine();
+            for(int i=0; i<n ; i++){
+                String s = in.nextLine();
+                StringBuilder sbOdd = new StringBuilder("");
+                StringBuilder sbEven = new StringBuilder("");
+                for(int j=0; j<s.length(); j++){
+                        if(j%2 == 0){
+                            sbEven.append(s.charAt(j));
+                        } else {
+                            sbOdd.append(s.charAt(j));
+                        }  
+                }
+                System.out.println(sbEven + " " + sbOdd);
+            }
+    }
+}
