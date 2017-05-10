f193806bc6f792c3da80dcb051d25ce5802b21a4
Omri Gotlieb <omrigotlieb@gmail.com>
2017-05-09 21:00:57 -0700

    MaximumDraws and HandShake Solutions added

---
+++ Mathematics/MaximumDraws/Solution.java
@@ -0,0 +1,23 @@
+//     Author: Omri Gotlieb
+//     Github: https://github.com/Omrigotlieb
+//     HackerRank: https://www.hackerrank.com/omrigotlieb
+//     Portfolio: http://omrigotlieb.com/
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int T = in.nextInt();
+        for(int i = 0; i < T; i++){
+            int N = in.nextInt();
+            System.out.println(N + 1);
+        }
+    }
+}
